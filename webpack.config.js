297d596a90d803a172232e570e31db24e9f080e4
Steve Creswick <screswick@gmail.com>
2016-01-07 01:22:50 -0500

    adds Hello World

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+
+module.exports = {
+// Entry acceps a path or an object of entries
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+
+  ]
+
+};
